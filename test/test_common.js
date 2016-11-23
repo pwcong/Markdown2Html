bde2569057771029f4f7d03c991f6aa469c56c07
Pwcong <pwcong@foxmail.com>
2016-11-23 12:01:56 +0800

    搭建框架#3

--- test/test_common.js
+++ test/test_common.js
@@ -2,19 +2,25 @@
  * Created by Pwcong on 2016/11/22.
  */
 
-var fs = require('fs');
+// var fs = require('fs');
+//
+// fs.readFile("markdown",function (err, data) {
+//
+//     if(err)
+//         console.log(err.message);
+//     else{
+//         var str = data.toString();
+//         var strArray = str.split("\n");
+//         console.log(strArray.length);
+//         for(var i in strArray)
+//             console.log(strArray[i]);
+//     }
+//
+//
+// });
 
-fs.readFile("markdown",function (err, data) {
+var html = require('../lib/util/html');
 
-    if(err)
-        console.log(err.message);
-    else{
-        var str = data.toString();
-        var strArray = str.split("\n");
-        console.log(strArray.length);
-        for(var i in strArray)
-            console.log(strArray[i]);
-    }
-
-
-});
+var str = "abcabc";
+var res = str.match(/abc/);
+console.log(res);
